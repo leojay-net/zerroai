ed44b4f12dd9fde9266dcdc26d02b426c62040e9
Joseph Aleonomoh <102752992+leojay-net@users.noreply.github.com>
2024-05-05 23:54:55 +0000

    Update settings.py

--- zerroai/settings.py
+++ zerroai/settings.py
@@ -87,11 +87,7 @@ SPECTACULAR_SETTINGS = {
     'TITLE': '',
     'DESCRIPTION': '',
     'VERSION': '1.0.0',
-    'SERVE_INCLUDE_SCHEMA': False,
-    'SWAGGER_UI_DIST': 'SIDECAR',  
-    'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
-    'REDOC_DIST': 'SIDECAR',
-    'COMPONENT_SPLIT_REQUEST':True,
+    'SERVE_INCLUDE_SCHEMA': False
 }
 
 WSGI_APPLICATION = 'zerroai.wsgi.application'
